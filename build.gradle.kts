c1cd94a3202197d79816405328d38bd0d3ce6f26
Mikhail Yakushin <driver733@gmail.com>
2020-07-09 15:32:37 +0300

    docs: readme updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,10 @@ dependencies {
     implementation(kotlin("gradle-plugin", "1.3.72"))
     implementation(kotlin("allopen", "1.3.72"))
     implementation("io.freefair.gradle:lombok-plugin:5.0.0-rc6")
+
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.0")
+    testImplementation("io.kotest:kotest-runner-console-jvm:4.1.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.1.0")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
