98a3a3c3d9f5fe8eaebb66f4a17353867cbef253
Mikhail Yakushin <driver733@gmail.com>
2020-12-18 21:42:24 +0300

    fix: junitPlatform enabled for all projects
    BREAKING CHANGE: rarely used libs removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,13 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.4.10"))
-    implementation(kotlin("allopen", "1.4.10"))
+    implementation(kotlin("gradle-plugin", "1.4.21"))
+    implementation(kotlin("allopen", "1.4.21"))
     implementation("io.freefair.gradle:lombok-plugin:5.0.0-rc6")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.1")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.2")
 
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.3.0")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.3.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.3.2")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.3.2")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
